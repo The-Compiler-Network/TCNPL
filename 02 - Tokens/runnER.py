da6941561d870df1eb9af6b741623e995f635831
NelsonGomesNeto <ngn@ic.ufal.br>
2018-07-12 23:32:15 -0300

    Adding examples of code

--- 02 - Tokens/runnER.py
+++ 02 - Tokens/runnER.py
@@ -1,8 +1,8 @@
-import os
+import os, sys
 
 os.system("flex TCNPL.lex")
 os.system("gcc lex.yy.c -o ER")
 if os.name == 'nt':
-    os.system("ER < testCases")
+  os.system("ER < %s" % sys.argv[1])
 else:
-    os.system("./ER < testCases")
+  os.system("./ER < %s" % sys.argv[1])
